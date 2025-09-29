0e2ba73926bca509d76c4cb5e258803e1ae04932
uu7157 <116019430+uu7157@users.noreply.github.com>
2025-09-29 18:19:05 +0300

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,15 +1,18 @@
 import os
 import requests
 
-def upload_to_abyss(file_path: str, api_key: str, progress_callback=None):
+def upload_to_hydrax(file_path: str, api_key: str, progress_callback=None):
     url = f"http://up.hydrax.net/{api_key}"
 
     with open(file_path, "rb") as f:
-        # stream upload
-        files = {"file": (os.path.basename(file_path), f)}
-
+        files = {"file": (os.path.basename(file_path), f, "video/mp4")}
         response = requests.post(url, files=files)
-        response.raise_for_status()
 
-    data = response.json()
-    return data.get("slug")
+    print("Response text:", response.text)
+    response.raise_for_status()
+
+    try:
+        data = response.json()
+        return data.get("url") or data.get("slug")
+    except Exception:
+        return None
